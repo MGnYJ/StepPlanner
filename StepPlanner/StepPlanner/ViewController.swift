1b7f82c38ff0ded29b62937d1b2bca1a251eb20b
tjfqhd <kunz19870414@gmail.com>
2017-02-05 14:03:44 +0900

    프로젝트 추가

---
+++ StepPlanner/StepPlanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StepPlanner
+//
+//  Created by Myoung gun Ahn on 2017. 2. 5..
+//  Copyright © 2017년 Myoung gun Ahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
