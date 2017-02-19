a8483cca53b5a923cd080c589ff827cb8e03b0c7
Myoung gun Ahn <tjfqhd_2000@naver.com>
2017-02-19 15:04:00 +0900

    소스 수

---
+++ StepPlanner/StepPlanner/HistoryPage.swift
@@ -0,0 +1,35 @@
+//
+//  HistoryPage.swift
+//  StepPlanner
+//
+//  Created by Myoung gun Ahn on 2017. 2. 19..
+//  Copyright © 2017년 Myoung gun Ahn. All rights reserved.
+//
+
+import UIKit
+
+class HistoryPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
